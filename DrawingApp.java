2717256b9d63f6697608343b5a63b2a8e7aa1ff3
OmarMohsen <omarmohsenabdallah@gmail.com>
2023-07-30 21:43:55 +0300

    create variables and add default values

--- DrawingApp.java
+++ DrawingApp.java
@@ -15,21 +15,24 @@
 
 
 public class DrawingApp extends JFrame {
-	
-	
-	
-	
-	
+	private String selectedShape;
+    private Color selectedColor;
+    private boolean filledShape;
+    private int startX, startY, endX, endY;
+    private static ArrayList<ShapeInfo> shapesList;
+    private int strokeSize;
 	
 	
 	
 	
 	
 	 public DrawingApp() {
+		//default values
+        selectedShape = "line";
+        selectedColor = Color.BLACK;
+        filledShape = false;
+        shapesList = new ArrayList<>();
 		
-
-
-
 		
 		
 		
@@ -398,10 +401,7 @@ public class DrawingApp extends JFrame {
 		
 		
 		
-		
-		
-		
-		
+			
 	 }
 	 
 }
