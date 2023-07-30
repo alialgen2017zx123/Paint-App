331612a59a3ccbcfcff2814920cc4e34fe167185
ALI MOHAMED <138809546+alialgen2017zx123@users.noreply.github.com>
2023-07-30 21:53:34 +0300

    Improve graphic quality

--- DrawingApp.java
+++ DrawingApp.java
@@ -50,7 +50,13 @@ public class DrawingApp extends JFrame {
 		
 		
 		
-		
+				//Improve graphic quality
+               g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);   
+               g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+               g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+               g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+               g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+               g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,1.0f));	
 		
 		
 		
